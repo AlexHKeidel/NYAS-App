8713741766d7f27afaffa991c571d7f5324fdbe4
Borgelman <alexander.keidel@go.edgehill.ac.uk>
2015-04-26 18:38:58 +0100

    Getting Date from Calendar Added
    Currently Printed into console when pressing "Make Appointment" button
    on Calendar Activity.

--- CalendarActivity.cs
+++ CalendarActivity.cs
@@ -16,6 +16,8 @@ namespace NYASApp
 	[Activity (Label = "CalendarActivity", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]			
 	public class CalendarActivity : Activity
 	{
+		int Day, Month, Year;
+
 		CalendarView Calendar;
 		Button MakeAppointmentButton;
 		protected override void OnCreate (Bundle bundle)
@@ -25,6 +27,7 @@ namespace NYASApp
 			SetContentView (Resource.Layout.Calendar_Activity_Layout); //setting layout
 			SetupResources(); //setting up all resource files to the defined objects
 			SetupLayout(); //setting up the layout of the activity
+			SetupButtonListeners();
 		}
 
 		private void SetupResources(){
@@ -37,7 +40,14 @@ namespace NYASApp
 			Calendar.SetPadding(0,0,0, (metrics.HeightPixels * 1/3));
 			var dateText = Calendar.GetChildAt (0); //position of the actual background of the calendar
 			dateText.SetBackgroundColor (Resources.GetColor (Resource.Color.calendarBackground));
+		}
 
+		private void SetupButtonListeners(){ 
+			MakeAppointmentButton.Click += delegate {
+				DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc); // see http://stackoverflow.com/questions/4964634/how-to-convert-long-type-datetime-to-datetime-with-correct-time-zone
+				DateTime converter = dt.AddMilliseconds(Calendar.Date).ToLocalTime();
+				Console.WriteLine(converter.ToShortDateString());
+			};
 		}
 	}
 }
